2c57aa853b1355c876b20121e57a5db9fcb2c6c7
Vlad <vlad300820001@gmail.com>
2020-05-10 12:12:10 +0300

    initial commit

---
+++ src/main/java/com/example/arithmetic_testing/ArithmeticTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.arithmetic_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArithmeticTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArithmeticTestingApplication.class, args);
+    }
+
+}
